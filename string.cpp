88e896a0d11216c35c73520e38d836100a9269ac
mtr_suraj <89896051+surajrepository@users.noreply.github.com>
2022-07-08 09:47:19 +0530

    mtr
    shrink_to_fit use to decrease the capacity of string

--- string.cpp
+++ string.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 
 int main(){
-    string str = "who's you";
+    string str = "who's you some time we face problem";
     cout << str << endl;
-    str.resize(4);
+    str.resize(7);
     cout << str << endl;
 
     //Display chapacity of the string
     cout << str.capacity() << endl;
     //length depend on resize if you use 
     cout << str.length() << endl;
+    // display of shrink_to_fit
+     str.shrink_to_fit();
+ //After the Shrink
+ cout << str.capacity() << endl;
      return 0;
 
 
